772b0c9ae2c8ba2bcae9e15936c2ea3187730c25
Mateus Henrique <mateusdzn10@gmail.com>
2024-04-11 00:21:02 -0300

    feat: add checkout feature

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CheckoutsModule } from './checkouts/checkouts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import {
+  Checkout,
+  CheckoutItem,
+  CheckoutProduct,
+} from './checkouts/entities/checkout.entity';
 
+//decorator - ES7
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'nest',
+      entities: [Checkout, CheckoutItem, CheckoutProduct],
+      synchronize: true,
+      logging: true,
+    }),
+    CheckoutsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
